11a826418c311163873879879913fc219d63451f
Stryders7 <fa.santibanez@duocuc.cl>
2023-08-11 22:22:58 -0400

    primer commit con ionic

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AlertComponent } from './alert/alert.component';
+import { ButtonComponent } from './button/button.component';
+import { LoginModule } from './login/login.module';
+
+
+@NgModule({
+  declarations: [AppComponent, AlertComponent, ButtonComponent,LoginModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
